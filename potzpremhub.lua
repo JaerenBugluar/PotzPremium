a1a326db7c5caf40b6c285f0c87467edf1741e6a
JaerenBugluar <159459193+JaerenBugluar@users.noreply.github.com>
2024-04-06 00:25:39 -0700

    potzpremhub

--- potzpremhub.lua
+++ potzpremhub.lua
@@ -12,27 +12,6 @@ local Window = Fluent:CreateWindow({
     MinimizeKey = Enum.KeyCode.End -- Used when theres no MinimizeKeybind
 })
 
-local L_91_ = Instance.new("ScreenGui")
-local L_92_ = Instance.new("ImageButton")
-local L_93_ = Instance.new("UICorner")
-L_91_.Name = "ToggleUI"
-L_91_.Parent = game.CoreGui
-L_91_.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-L_92_.Name = "ToggleButton"
-L_92_.Parent = L_91_
-L_92_.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-L_92_.BorderSizePixel = 0
-L_92_.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
-L_92_.Size = UDim2.new(0, 50, 0, 50)
-L_92_.BackgroundTransparency = 1.000
-L_92_.Image = "rbxassetid://17015092259"
-L_92_.Draggable = true
-L_92_.MouseButton1Click:Connect(function()
-    game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.End, false, game)
-end)
-L_93_.CornerRadius = UDim.new(0, 15)
-L_93_.Parent = L_92_
-
 SaveManager:SetLibrary(Fluent)
 InterfaceManager:SetLibrary(Fluent)
 SaveManager:IgnoreThemeSettings()
